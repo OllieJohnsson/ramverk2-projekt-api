c92476d9df8501ec165fa6696054c2cecda9e386
Oliver <oliver.johnsson@me.com>
2019-03-05 09:01:15 +0100

    Updated db config

--- db/connection.js
+++ db/connection.js
@@ -1,24 +1,24 @@
 const mysql = require('mysql');
 
-switch (process.env.NODE_ENV) {
-    case "development":
-        return module.exports = mysql.createPool({
-            connectionLimit: 10,
-            host: 'localhost',
-            user: process.env.MYSQL_USER,
-            password: process.env.MYSQL_PASS,
-            database: 'rv2proj'
-        });
-        break;
-
-    case "production":
-        return module.exports = mysql.createPool({
-            connectionLimit: 10,
-            host: 'eu-cdbr-west-02.cleardb.net',
-            user: process.env.MYSQL_USER,
-            password: process.env.MYSQL_PASS,
-            database: 'heroku_a840f59c1a1e794'
-        });
-        break;
+// switch (process.env.NODE_ENV) {
+    // case "development":
+return module.exports = mysql.createPool({
+    connectionLimit: 10,
+    host: 'localhost',
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASS,
+    database: 'rv2proj'
+});
+        // break;
+    //
+    // case "production":
+    //     return module.exports = mysql.createPool({
+    //         connectionLimit: 10,
+    //         host: 'eu-cdbr-west-02.cleardb.net',
+    //         user: process.env.MYSQL_USER,
+    //         password: process.env.MYSQL_PASS,
+    //         database: 'heroku_a840f59c1a1e794'
+    //     });
+    //     break;
 
 }
