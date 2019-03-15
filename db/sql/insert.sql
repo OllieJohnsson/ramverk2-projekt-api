866ed7d795201fbd5eb0bb0e058779956a69fa2e
OllieJohnsson <oliver.johnsson@me.com>
2019-03-15 12:23:39 +0100

    Updated insert.sql

--- db/sql/insert.sql
+++ db/sql/insert.sql
@@ -1,3 +1,8 @@
-INSERT INTO objects (name, price, stock, rate, variance) VALUES ("kaffe", 100, 100, 1.002, 0.6), ("kakor", 12.50, 100, 1.001, 0.3);
+INSERT INTO objects (name, price, stock, rate, variance)
+VALUES
+("kaffe", 100, 100, 1.002, 0.6),
+("kakor", 12.50, 100, 1.001, 0.3),
+("godis", 8, 1000, 1.001, 0.3)
+;
 
 CALL register('olliej', '$2b$10$uvxjNQiUczgc0OCqz7PeZe.ztfMtLyef1CQ2Nt1zKL8uF08bn9.5W', 'oliver@me.com', 'Oliver', 'Johnsson');
