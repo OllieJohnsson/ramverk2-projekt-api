449ae1817837c28b7cabf5cfee3374f92fceead6
Oliver <oliver.johnsson@me.com>
2019-03-21 13:11:28 +0100

    Updated object icons

--- db/sql/insert.sql
+++ db/sql/insert.sql
@@ -1,11 +1,11 @@
-INSERT INTO objects (name, price, stock, rate, variance)
+INSERT INTO objects (name, icon, price, stock, rate, variance)
 VALUES
-("Kaffe ☕️", 30, 100, 1.001, 0.6),
-("Pannkakor 🥞", 25, 20, 1.001, 0.3),
-("Croissant 🥐", 20, 20, 1.001, 0.2),
-("Bagel 🥯", 15, 30, 1.001, 0.1),
-("Kringla 🥨", 15, 30, 1.001, 0.1),
-("Kaka 🍪", 10, 30, 1.001, 0.05)
+("Kaffe", "☕️", 30, 100, 1.001, 0.6),
+("Pannkakor", "🥞", 25, 20, 1.001, 0.3),
+("Croissant", "🥐", 20, 20, 1.001, 0.2),
+("Bagel", "🥯", 15, 30, 1.001, 0.1),
+("Kringla", "🥨", 15, 30, 1.001, 0.1),
+("Kaka", "🍪", 10, 30, 1.001, 0.05)
 ;
 
 CALL register('olliej', '$2b$10$uvxjNQiUczgc0OCqz7PeZe.ztfMtLyef1CQ2Nt1zKL8uF08bn9.5W', 'oliver@me.com', 'Oliver', 'Johnsson');
